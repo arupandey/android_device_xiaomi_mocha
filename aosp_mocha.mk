be283d143b6a3a5b284487f9e1c2901ccba6b8d2
Moto g Multirom <bhavy.singhal3@gmail.com>
2015-06-30 08:20:36 +0200

    Update and rename cm.mk to aosp_mocha.mk

---
+++ aosp_mocha.mk
@@ -0,0 +1,13 @@
+# Release name
+PRODUCT_RELEASE_NAME := mocha
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/mocha/mocha.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mocha
+PRODUCT_NAME := aosp_mocha
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := mocha
+PRODUCT_MANUFACTURER := xiaomi
+
